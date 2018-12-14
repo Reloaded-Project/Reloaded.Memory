4d3274f44768c483e6f575fe42968ee316646fb1
sewer56lol <sewer56lol@googlemail.com>
2018-12-14 18:46:01 +0000

    [Memory] Fix potential hang on ChangePermission() (Current Process)

--- Source/Reloaded.Memory/Memory/Sources/Memory.cs
+++ Source/Reloaded.Memory/Memory/Sources/Memory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
@@ -49,12 +49,33 @@ namespace Reloaded.Memory.Sources
         /// <inheritdoc />
         public IntPtr  Allocate(int length)
         {
-            return Marshal.AllocHGlobal(length);
+            // DO NOT USE Marshal.AllocHGlobal (GlobalAlloc)
+            // Using AllocHGlobal will allocate our memory in a page that may be shared with other content; 
+            // this may cause tests to fail when ChangePermission() is executed.
+
+            /*
+                MSDN: It is best to avoid using VirtualProtect to change page protections on memory blocks allocated by GlobalAlloc, 
+                HeapAlloc, or LocalAlloc, because multiple memory blocks can exist on a single page. The heap manager assumes that all
+                pages in the heap grant at least read and write access.
+            */
+
+            IntPtr returnAddress = Kernel32.VirtualAlloc
+            (
+                IntPtr.Zero,
+                (uint)length,
+                Kernel32.MEM_ALLOCATION_TYPE.MEM_COMMIT | Kernel32.MEM_ALLOCATION_TYPE.MEM_RESERVE,
+                Kernel32.MEM_PROTECTION.PAGE_EXECUTE_READWRITE
+            );
+
+            if (returnAddress == IntPtr.Zero)
+                throw new MemoryAllocationException($"Failed to allocate memory in current process: {length} bytes, {Kernel32.GetLastError()} last error.");
+
+            return returnAddress;
         }
         /// <inheritdoc />
         public bool    Free(IntPtr address)
         {
-            Marshal.FreeHGlobal(address);
+            Kernel32.VirtualFree(address, 0, Kernel32.MEM_ALLOCATION_TYPE.MEM_DECOMMIT | Kernel32.MEM_ALLOCATION_TYPE.MEM_RELEASE);
             return true;
         }
 
@@ -67,7 +88,7 @@ namespace Reloaded.Memory.Sources
         /// <inheritdoc />
         public Kernel32.MEM_PROTECTION ChangePermission(IntPtr memoryAddress, int size, Kernel32.MEM_PROTECTION newPermissions)
         {
-            bool result = Kernel32.VirtualProtect(memoryAddress, (uint)size, newPermissions, out var oldPermissions);
+            bool result = Kernel32.VirtualProtect(memoryAddress, (uint)size, newPermissions, out Kernel32.MEM_PROTECTION oldPermissions);
 
             if (!result)
                 throw new MemoryPermissionException($"Unable to change permissions for the following memory address {memoryAddress.ToString("X")} of size {size} and permission {newPermissions.ToString()}");
