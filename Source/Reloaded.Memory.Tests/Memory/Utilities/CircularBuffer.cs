c31125936da9dc33c4beb0e0f6a76cb71ed7914b
Sewer56 <sewer56lol@googlemail.com>
2018-12-31 21:40:13 +0000

    Implement New Feature: Circular Buffers

---
+++ Source/Reloaded.Memory.Tests/Memory/Utilities/CircularBuffer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Reloaded.Memory.Tests.Memory.Helpers;
+using Xunit;
+
+namespace Reloaded.Memory.Tests.Memory.Utilities
+{
+    public class CircularBuffer
+    {
+
+        /// <summary>
+        /// Extreme test which just about only fills the buffer, tests
+        /// whether the buffer looped, then loops the buffer and validates
+        /// pointer reset.
+        ///
+        /// Because <see cref="Reloaded.Memory.Utilities.CircularBuffer"/> is fairly small, it also tests
+        /// all other functionality of the buffer along the way.
+        /// </summary>
+        [Fact]
+        public void TestBuffer()
+        {
+            // Setup
+            int bufferSize = 4096;
+            var memory = new Reloaded.Memory.Sources.Memory();
+            var buffer = new Reloaded.Memory.Utilities.CircularBuffer(bufferSize, memory);
+
+            // Properties.
+            int structSize     = Struct.GetSize<RandomIntStruct>();
+            int bufferElements = bufferSize / structSize;
+
+            var randomIntStructs = new RandomIntStruct[bufferElements];
+            for (int x = 0; x < bufferElements; x++)
+                randomIntStructs[x] = RandomIntStruct.BuildRandomStruct();
+
+            // Fill circular buffer and validate.
+            for (int x = 0; x < bufferElements; x++)
+            {
+                // Save write pointer.
+                IntPtr nextPointer = buffer.WritePointer + structSize;
+
+                var address = buffer.Add(ref randomIntStructs[x]);
+                memory.Read(address, out RandomIntStruct randIntStruct);
+
+                // Check correct element and pointer did not loop.
+                Assert.Equal(randomIntStructs[x], randIntStruct);
+                Assert.Equal(nextPointer, buffer.WritePointer);
+            }
+
+            // Now check if item can fit, it should not.
+            // Buffer should require looping.
+            var randomIntStruct = RandomIntStruct.BuildRandomStruct();
+            var canFit = buffer.CanItemFit(ref randomIntStruct);
+
+            Assert.Equal(Reloaded.Memory.Utilities.CircularBuffer.ItemFit.StartOfBuffer, canFit);
+
+            // Add item to loop.
+            var lastItemPtr = buffer.Add(ref randomIntStruct);
+            Assert.Equal(buffer.Address, lastItemPtr); // New item should have been placed at offset 0.
+
+            // Item too big.
+            buffer.Offset = 0;
+            byte[] sameSizeAsBuffer = new byte[bufferSize];
+            byte[] tooBigForBuffer = new byte[bufferSize + 1];
+
+            // Will not fit.
+            Assert.Equal(Reloaded.Memory.Utilities.CircularBuffer.ItemFit.No, buffer.CanItemFit(tooBigForBuffer.Length));
+            Assert.Equal(IntPtr.Zero, buffer.Add(tooBigForBuffer));
+
+            // Will fit in current loop. Note Offset = 0.
+            Assert.Equal(Reloaded.Memory.Utilities.CircularBuffer.ItemFit.Yes, buffer.CanItemFit(sameSizeAsBuffer.Length));
+            Assert.NotEqual(IntPtr.Zero, buffer.Add(sameSizeAsBuffer));
+            
+            buffer.Offset = 1; // Will fit on next loop.
+            Assert.Equal(Reloaded.Memory.Utilities.CircularBuffer.ItemFit.StartOfBuffer, buffer.CanItemFit(sameSizeAsBuffer.Length));
+            Assert.NotEqual(IntPtr.Zero, buffer.Add(sameSizeAsBuffer));
+            
+            // Cleanup
+            buffer.Dispose();
+        }
+    }
+}
---
+++ Source/Reloaded.Memory/Memory/Utilities/CircularBuffer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Reloaded.Memory.Sources;
+
+namespace Reloaded.Memory.Utilities
+{
+    /// <summary>
+    /// The <see cref="CircularBuffer"/> is a writeable buffer useful for temporary storage of data.
+    /// It's a buffer whereby once you reach the end of the buffer, it loops back over to the beginning of the stream automatically.
+    /// </summary>
+    public class CircularBuffer : IDisposable
+    {
+        /// <summary>
+        /// Access to memory source where the <see cref="CircularBuffer"/> reads from/writes to.
+        /// </summary>
+        public IMemory  Source    { get; }
+
+        /// <summary>
+        /// Current offset of next item within the <see cref="CircularBuffer"/>.
+        /// </summary>
+        public int      Offset    { get; set; }
+
+        /// <summary>
+        /// The address of the <see cref="CircularBuffer"/>.
+        /// </summary>
+        public IntPtr   Address   { get; set; }
+
+        /// <summary>
+        /// Size of the <see cref="CircularBuffer"/>.
+        /// </summary>
+        public int      Size      { get; set; }
+
+        /// <summary>
+        /// Returns the address of where the next element will be written onto the buffer.
+        /// </summary>
+        public IntPtr   WritePointer => Address + Offset;
+
+        /// <summary> Remaining space until the Circular buffer next loops. </summary>
+        private int     Remaining    => Size - Offset;
+
+        /// <summary>
+        /// Creates a <see cref="CircularBuffer"/> within the target memory source.
+        /// </summary>
+        /// <param name="size">The size of the buffer in bytes.</param>
+        /// <param name="source">The source of the buffer where to read/write elements to/from.</param>
+        public CircularBuffer(int size, IMemory source)
+        {
+            Offset      = 0;
+            Size        = size;
+            Source      = source;
+            Address     = source.Allocate(Size);
+        }
+
+
+        /* Public Interface */
+
+        /// <summary>
+        /// Adds a new item onto the circular buffer.
+        /// </summary>
+        /// <param name="bytesToWrite">The bytes to add onto the buffer.</param>
+        /// <returns>Pointer to the recently added item to the buffer, or IntPtr.Zero if the item cannot fit.</returns>
+        public IntPtr Add(byte[] bytesToWrite)
+        {
+            var canFit = CanItemFit(bytesToWrite.Length);
+
+            if (canFit == ItemFit.No)
+                return IntPtr.Zero;
+
+            if (canFit == ItemFit.StartOfBuffer)
+                Offset = 0;
+
+            IntPtr writeAddress = WritePointer;
+            Source.WriteRaw(writeAddress, bytesToWrite);
+
+            Offset += bytesToWrite.Length;
+            return writeAddress;
+        }
+
+        /// <summary>
+        /// Adds a new item onto the circular buffer.
+        /// </summary>
+        /// <param name="item">The item to add onto the buffer.</param>
+        /// <param name="marshalElement">The element to be marshalled.</param>
+        /// <returns>Pointer to the recently added item to the buffer.</returns>
+        public IntPtr Add<TStructure>(ref TStructure item, bool marshalElement = false)
+        {
+            return Add(Struct.GetBytes(ref item, marshalElement));
+        }
+
+        /// <summary>
+        /// Returns an enumerable describing if an item can fit into the buffer.
+        /// </summary>
+        /// <param name="objectSize">The size of the object to be appended to the buffer.</param>
+        public ItemFit CanItemFit(int objectSize)
+        { 
+            if (Remaining >= objectSize)
+                return ItemFit.Yes;
+
+            if (Size >= objectSize)
+                return ItemFit.StartOfBuffer;
+
+            return ItemFit.No;
+        }
+
+        /// <summary>
+        /// Returns an enumerable describing if an item can fit into the buffer.
+        /// </summary>
+        /// <param name="item">The item to check if it can fit into the buffer.</param>
+        /// <param name="marshalElement">True if the item is to be marshalled, else false.</param>
+        public ItemFit CanItemFit<TStructure>(ref TStructure item, bool marshalElement = false)
+        {
+            return CanItemFit(Struct.GetSize<TStructure>(marshalElement));
+        }
+
+        /* Custom Types. */
+
+        /// <summary/>
+        public enum ItemFit
+        {
+            /// <summary> The item is too large to fit into the buffer. </summary>
+            No,
+            /// <summary> The item can fit into the buffer. </summary>
+            Yes,
+            /// <summary> The item can fit into the buffer, but not in the remaining space (will be placed at start of buffer).</summary>
+            StartOfBuffer
+        }
+
+        /* Overrides. */
+
+        /// <summary/>
+        public void Dispose()
+        {
+            Source.Free(Address);
+        }
+    }
+}
