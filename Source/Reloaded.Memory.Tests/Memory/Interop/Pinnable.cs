5a673fe6e005bf4b0fd7e9e3df1289dfc5bfbf8b
Sewer <seweratwork@gmail.com>
2019-09-07 17:24:50 +0100

    Pinnable: Add tests for pinning arrays.

--- Source/Reloaded.Memory.Tests/Memory/Interop/Pinnable.cs
+++ Source/Reloaded.Memory.Tests/Memory/Interop/Pinnable.cs
@@ -22,6 +22,22 @@ namespace Reloaded.Memory.Tests.Memory.Interop
             pinnable.Dispose();
         }
 
+        [Fact]
+        public unsafe void PinArray()
+        {
+            int[] integers = { 0, 1, 2, 3, 4, 5, 6 };
+            var pinnable = new Pinnable<int>(integers);
+
+            // Integers are on the heap, should be pinned by reference.
+            for (int x = 0; x < integers.Length; x++)
+            {
+                integers[x] = 100 + x;
+                Assert.Equal(integers[x], pinnable.Pointer[x]);
+            }
+
+            pinnable.Dispose();
+        }
+
         // Pinnable<T> should not dispose native object, only PinnableDisposable<T>.
         [Fact]
         public void NoDispose()
