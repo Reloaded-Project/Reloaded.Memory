19d8f73b3987f53aff3cb72c5eed43df37cee71e
Sewer 56 <seweratwork@gmail.com>
2022-03-15 09:15:18 +0000

    Added: Trimmable Flag & Testing for Trimming

--- Source/Reloaded.Memory.Benchmark/Program.cs
+++ Source/Reloaded.Memory.Benchmark/Program.cs
@@ -7,6 +7,8 @@ namespace Reloaded.Memory.Benchmark
     {
         public static void Main(string[] args)
         {
+            // Trimmer Test: dotnet publish -c Release -f net6.0-windows -r win-x64 -p:PublishTrimmed=true -p:TrimmerDefaultAction=linkC:\Users\sewer\Desktop\Projects\Reloaded.Memory\Source\Reloaded.Memory.Benchmark\Reloaded.Memory.Benchmark.csproj
+
             //BenchmarkRunner.Run<Memory.Streams.SmallStruct.FileStream>();
             //BenchmarkRunner.Run<Memory.Streams.SmallStruct.MemoryStream>();
 
